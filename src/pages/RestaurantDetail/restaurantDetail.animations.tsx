444a158c8724d3a59ed939af1a8b522505231cc5
Walter Lambardi <walterlambardi@MMBA-B569.local>
2023-05-03 17:37:34 -0300

    Add type fix to restaurant detail animation

--- src/pages/RestaurantDetail/restaurantDetail.animations.tsx
+++ src/pages/RestaurantDetail/restaurantDetail.animations.tsx
@@ -1,20 +1,21 @@
+import { Animated } from 'react-native';
 import { HEADER_HEIGHT } from './restaurantDetail.style';
 
-export const headerHeightProgress = (scrollY: any) =>
+export const headerHeightProgress = (scrollY: Animated.Value) =>
   scrollY.interpolate({
     inputRange: [0, HEADER_HEIGHT],
     outputRange: [HEADER_HEIGHT, HEADER_HEIGHT / 1.6],
     extrapolate: 'clamp',
   });
 
-export const titleOpacity = (scrollY: any) =>
+export const titleOpacity = (scrollY: Animated.Value) =>
   scrollY.interpolate({
     inputRange: [0, HEADER_HEIGHT],
     outputRange: [0, 1],
     extrapolate: 'clamp',
   });
 
-export const imageOverlarOpacity = (scrollY: any) =>
+export const imageOverlarOpacity = (scrollY: Animated.Value) =>
   scrollY.interpolate({
     inputRange: [0, HEADER_HEIGHT],
     outputRange: [0.25, 0.8],
